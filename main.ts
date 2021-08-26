5c513e6ffc087932b1516373397ecb8e869ae56c
GitMurf <64155612+GitMurf@users.noreply.github.com>
2021-08-26 15:07:57 -0700

    v0.1 for initial testing

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, WorkspaceLeaf, MarkdownView, Editor } from 'obsidian';
+declare module "obsidian" {
+    interface WorkspaceLeaf {
+        containerEl: HTMLElement;
+    }
+    interface Editor {
+        posAtCoords(left: number, top: number): EditorPosition;
+    }
+}
+const pluginName = 'Drag and Drop Blocks';
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -9,76 +18,235 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    settings: MyPluginSettings;
+
+    //My variables
+    blockRefStartLine: number;
+    blockRefEmbed: string;
+    blockRefNewLine: string;
+    originalText: string;
+    blockRefDragState: string;
+    blockRefStartLeaf: WorkspaceLeaf;
+    blockRefClientY: number;
+    blockRefModDrag: {
+        alt: boolean,
+        ctrl: boolean,
+        shift: boolean
+    }
+    blockRefModDrop: {
+        alt: boolean,
+        ctrl: boolean,
+        shift: boolean
+    }
 
 	async onload() {
-		console.log('loading plugin');
+        console.log("loading plugin: " + pluginName);
+
+        this.blockRefStartLine = null;
+        this.blockRefEmbed = null;
+        this.blockRefNewLine = null;
+        this.originalText = null;
+        this.blockRefDragState = null;
+        this.blockRefStartLeaf = null;
+        this.blockRefClientY = null;
+        this.blockRefModDrop = { alt: null, ctrl: null, shift: null }
+        this.blockRefModDrag = { alt: null, ctrl: null, shift: null }
 
 		await this.loadSettings();
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+        this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		this.addStatusBarItem().setText('Status Bar Text');
+        this.app.workspace.onLayoutReady(this.onLayoutReady.bind(this));
+    }
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+    onLayoutReady(): void {
+        //Find the main DIV that holds all the markdown panes
+        const actDoc: HTMLElement = document.getElementsByClassName('workspace-split mod-vertical mod-root')[0] as HTMLElement;
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        this.registerDomEvent(actDoc, 'mouseover', (evt: MouseEvent) => {
+            const mainDiv: HTMLElement = evt.target as HTMLElement;
+            if (mainDiv.className === 'CodeMirror-linenumber CodeMirror-gutter-elt') {
+                let oldElem: HTMLElement = document.getElementById('block-ref-hover');
+                if (oldElem) { oldElem.remove() }
+                let docBody: HTMLBodyElement = document.getElementsByTagName('body')[0];
+                const newElement: HTMLDivElement = document.createElement("div");
+                newElement.id = 'block-ref-hover';
+                docBody.insertBefore(newElement, null);
+                newElement.draggable = true;
+                //newElement.innerText = "◎";
+                //newElement.innerText = "❖";
+                //newElement.style.fontSize = "12px";
+                newElement.innerText = "⋮⋮";
+                newElement.style.fontSize = "16px";
+                newElement.style.fontWeight = "bold";
+                newElement.style.color = "var(--text-accent-hover)";
+                //newElement.style.cursor = "grab";
+                newElement.style.cursor = "move";
+                newElement.style.position = "absolute";
+                let targetRect = mainDiv.getBoundingClientRect();
+                newElement.style.top = `${targetRect.top - 1}px`;
+                newElement.style.left = `${targetRect.left - 8}px`;
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
+                this.registerDomEvent(newElement, 'mouseover', (evt: MouseEvent) => {
+                    const eventDiv: HTMLElement = evt.target as HTMLElement;
+                    eventDiv.style.color = "var(--text-accent-hover)";
+                })
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+                this.registerDomEvent(newElement, 'mouseout', (evt: MouseEvent) => {
+                    const eventDiv: HTMLElement = evt.target as HTMLElement;
+                    eventDiv.style.color = "transparent";
+                })
 
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+                this.registerDomEvent(mainDiv, 'mouseout', (evt: MouseEvent) => {
+                    const oldElem: HTMLElement = document.getElementById('block-ref-hover');
+                    if (oldElem) { oldElem.style.color = "transparent"; }
+                })
+
+                //Find the leaf that is being hovered over
+                let leafEl = this.app.workspace.containerEl.find(".workspace-leaf:hover");
+                let allLeaves: Array<WorkspaceLeaf> = this.app.workspace.getLeavesOfType("markdown");
+                let hoveredLeaf: WorkspaceLeaf = allLeaves.find(eachLeaf => eachLeaf.containerEl == leafEl);
+                if (hoveredLeaf) {
+                    this.blockRefStartLeaf = hoveredLeaf;
+                    this.blockRefClientY = evt.clientY + 1;
+                }
+
+                this.registerDomEvent(newElement, 'dragstart', (evt: DragEvent) => {
+                    let hoveredLeaf: WorkspaceLeaf = this.blockRefStartLeaf;
+                    let mdView: MarkdownView;
+                    if (hoveredLeaf) { mdView = hoveredLeaf.view as MarkdownView; }
+                    if (mdView) {
+                        this.blockRefModDrag = { alt: evt.altKey, ctrl: (evt.ctrlKey || evt.metaKey), shift: evt.shiftKey }
+                        let mdEditor: Editor = mdView.editor;
+                        let topPos: number = this.blockRefClientY;
+                        //NOTE: mdEditor.posAtCoords(x, y) is equivalent to mdEditor.cm.coordsChar({ left: x, top: y })
+                        let thisLine: number = mdEditor.posAtCoords(0, topPos).line;
+                        //mdEditor.setSelection({ line: thisLine, ch: 0 }, { line: thisLine, ch: 9999 });
+                        let lineContent: string = mdEditor.getLine(thisLine);
+
+                        let blockid: string = '';
+                        let finalString: string = '';
+                        let block: string = '';
+
+                        //No modifier keys held so move the block to the new location
+                        if (!this.blockRefModDrag.ctrl && !this.blockRefModDrag.alt && !this.blockRefModDrag.shift) {
+                            evt.dataTransfer.setData("text/plain", lineContent);
+                        }
+
+                        //Shift key held so copy the block to the new location
+                        if (!this.blockRefModDrag.ctrl && !this.blockRefModDrag.alt && this.blockRefModDrag.shift) {
+                            evt.dataTransfer.setData("text/plain", lineContent);
+                        }
+
+                        //Ctrl key held to create a block reference
+                        if (this.blockRefModDrag.ctrl && !this.blockRefModDrag.alt && !this.blockRefModDrag.shift) {
+                            const blockRef: RegExpMatchArray = lineContent.match(/ \^(.*)/);
+                            if (blockRef) {
+                                blockid = blockRef[1];
+                                finalString = lineContent;
+                            } else {
+                                let characters: string = 'abcdefghijklmnopqrstuvwxyz0123456789';
+                                let charactersLength: number = characters.length;
+                                for (var i = 0; i < 7; i++) {
+                                    blockid += characters.charAt(Math.floor(Math.random() * charactersLength));
+                                }
+                                finalString = lineContent + ` ^${blockid}`;
+                            }
+
+                            block = `![` + `[${mdView.file.basename}#^${blockid}]]`.split("\n").join("");
+                            evt.dataTransfer.setData("text/plain", block);
+                        }
+
+                        this.blockRefStartLine = thisLine;
+                        this.blockRefEmbed = block;
+                        this.blockRefNewLine = finalString;
+                        this.originalText = lineContent;
+                        this.blockRefDragState = 'start';
+                    }
+                })
+
+                this.registerDomEvent(newElement, 'dragend', (evt: DragEvent) => {
+                    if (this.blockRefDragState === "dropped") {
+                        //this.app.workspace.setActiveLeaf(this.blockRefStartLeaf);
+                        let mdView: MarkdownView;
+                        if (this.blockRefStartLeaf) { mdView = this.blockRefStartLeaf.view as MarkdownView; }
+                        if (mdView) {
+                            let mdEditor: Editor = mdView.editor;
+
+                            //No modifier keys held so move the block to the new location
+                            if (!this.blockRefModDrag.ctrl && !this.blockRefModDrag.alt && !this.blockRefModDrag.shift) {
+                                //Delete the original line you dragged
+                                mdEditor.setLine(this.blockRefStartLine, '');
+                            }
+
+                            //Shift key held so copy the block to the new location
+                            if (!this.blockRefModDrag.ctrl && !this.blockRefModDrag.alt && this.blockRefModDrag.shift) {
+                                //Do not have to do anything to the original block you dragged because it is just a copy / duplicate command
+                            }
+
+                            //Ctrl key held to create a block reference
+                            if (this.blockRefModDrag.ctrl) {
+                                mdEditor.setLine(this.blockRefStartLine, this.blockRefNewLine);
+                                mdEditor.setSelection({ line: this.blockRefStartLine, ch: 0 }, { line: this.blockRefStartLine, ch: 9999 });
+                            }
+                        }
+                    }
+
+                    if (this.blockRefDragState === 'cancelled') {
+                        //Nothing right now
+                    }
+
+                    let oldElem = document.getElementById('block-ref-hover');
+                    if (oldElem) { oldElem.remove() }
+                    this.blockRefStartLine = null;
+                    this.blockRefEmbed = null;
+                    this.blockRefNewLine = null;
+                    this.originalText = null;
+                    this.blockRefDragState = null;
+                    this.blockRefStartLeaf = null;
+                    this.blockRefClientY = null;
+                    this.blockRefModDrop = { alt: null, ctrl: null, shift: null }
+                    this.blockRefModDrag = { alt: null, ctrl: null, shift: null }
+                })
+            }
+
+            if (evt.ctrlKey && evt.shiftKey) {
+                let leafEl = this.app.workspace.containerEl.find(".workspace-leaf:hover");
+
+                let allLeaves: Array<WorkspaceLeaf> = this.app.workspace.getLeavesOfType("markdown");
+                let hoveredLeaf: WorkspaceLeaf = allLeaves.find(eachLeaf => eachLeaf.containerEl == leafEl);
+                let mdView: MarkdownView;
+                if (hoveredLeaf) { mdView = hoveredLeaf.view as MarkdownView; }
+                if (mdView) {
+                    let mdEditor: Editor = mdView.editor;
+                    let topPos: number = evt.clientY + 1;
+                    //NOTE: mdEditor.posAtCoords(x, y) is equivalent to mdEditor.cm.coordsChar({ left: x, top: y })
+                    let thisLine: number = mdEditor.posAtCoords(0, topPos).line;
+                    mdEditor.setSelection({ line: thisLine, ch: 0 }, { line: thisLine, ch: 9999 });
+                }
+            }
+        });
+
+        this.registerDomEvent(actDoc, 'drop', (evt: DragEvent) => {
+            if (this.blockRefDragState === 'start') {
+                this.blockRefDragState = 'dropped';
+                this.blockRefModDrop = { alt: evt.altKey, ctrl: (evt.ctrlKey || evt.metaKey), shift: evt.shiftKey }
+            }
+        })
+    }
+
+    onunload() {
+        let oldElem = document.getElementById('block-ref-hover');
+        if (oldElem) { oldElem.remove() }
+        console.log("Unloading plugin: " + pluginName);
 	}
 
-	onunload() {
-		console.log('unloading plugin');
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
+    async saveSettings() {
+        await this.saveData(this.settings);
 	}
 }
 
@@ -95,16 +263,15 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+        containerEl.createEl('h2', { text: 'Drag and Drop Block Settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+            .setName('Setting 1')
+            .setDesc('This is a placeholder only and doesn\'t do anything at this point')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
+                .setPlaceholder('N/A')
 				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
+                .onChange(async (value) => {
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
